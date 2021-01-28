a42b958690c2f56d30b05cc250e95052626d4204
heboxaun <2423571560@qq.com>
2021-01-28 22:39:10 +0800

    补充缺失文件

---
+++ message_dao/src/main/java/A.java
@@ -0,0 +1,6 @@
+/**
+ * @Author: heboxuan
+ * @Date: Created in 22:37 2021/1/28
+ */
+public class A {
+}
---
+++ message_interface/src/main/java/com/he/service/A.java
@@ -0,0 +1,8 @@
+package com.he.service;
+
+/**
+ * @Author: heboxuan
+ * @Date: Created in 22:37 2021/1/28
+ */
+public class A {
+}
